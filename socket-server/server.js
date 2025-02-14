9196d845db868e596970fd1f52f8e18094155acd
Ivan Brilata <brilataivan86@gmail.com>
2025-02-14 14:13:30 +0800

    [add] real time

--- socket-server/server.js
+++ socket-server/server.js
@@ -12,23 +12,16 @@ const io = socketIo(server, {
     }
 });
 
-app.use(cors());
-app.use(express.json());
-
 io.on("connection", (socket) => {
     console.log("A user connected");
+
+    socket.on("newQueueItem", (data) => {
+        io.emit("updateQueue", data); // Broadcast to all clients
+    });
 
     socket.on("disconnect", () => {
         console.log("A user disconnected");
     });
-});
-
-// Receive queue item from CodeIgniter
-app.post("/newQueueItem", (req, res) => {
-    const data = req.body;
-    console.log("Received from CodeIgniter:", data);
-    io.emit("updateQueue", data); // Broadcast to all clients
-    res.sendStatus(200);
 });
 
 server.listen(3000, () => {
